54f69078857ff9c26008cccd50fbe39d81282284
Sushrit_Lawliet <sushrit.pk21@gmail.com>
2021-07-22 08:29:08 +0530

    Update DarkDiv.js

--- web/components/DarkDiv.js
+++ web/components/DarkDiv.js
@@ -2,9 +2,9 @@ import React from 'react'
 
 export default function DarkDiv() {
     return (
-        <div class="bg-white dark:bg-gray-800">
-            <h1 class="text-gray-900 dark:text-white">Dark mode is here!</h1>
-            <p class="text-gray-600 dark:text-gray-300">
+        <div className="bg-white dark:bg-gray-800">
+            <h1 className="text-gray-900 dark:text-white">Dark mode is here!</h1>
+            <p className="text-gray-600 dark:text-gray-300">
                 Lorem ipsum...
             </p>
         </div>
